2070b4460f4944d93d9191ab3d2eb52f317e2aa9
mmayeda <markrmayeda@gmail.com>
2021-01-15 11:54:59 -0800

    Added totems and added ambiance to musicmanager

--- The Soul Gaurd/Assets/MoonRift Teams/Programming AI StateMachine/Scripts/MusicManager.cs
+++ The Soul Gaurd/Assets/MoonRift Teams/Programming AI StateMachine/Scripts/MusicManager.cs
@@ -12,12 +12,14 @@ public class MusicManager : MonoBehaviour
         public float volume = 0.5f;
     }
 
-    AudioSource audioSource;
+    public AudioSource musicAudioSource;
+    public AudioSource ambianceAudioSource;
     public List<TrackTable> tracks = new List<TrackTable>();
+    public List<TrackTable> ambianceTracks = new List<TrackTable>();
 
     private void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
     }
 
     void Start()
@@ -28,8 +30,9 @@ public class MusicManager : MonoBehaviour
     public void ChangeTrackWithoutFade(string tag)
     {
         var clipToPlay = tracks.Find(tracks => tracks.trackName == tag);
-        audioSource.clip = clipToPlay.track;
-        audioSource.Play();
+        musicAudioSource.clip = clipToPlay.track;
+        musicAudioSource.Play();
+        musicAudioSource.volume = clipToPlay.volume;
     }
 
     public void ChangeTrackForState(string state)
@@ -40,16 +43,48 @@ public class MusicManager : MonoBehaviour
     public IEnumerator FadeOutAndPlayNextTrack(string state)
     {
         float currentTIme = 0;
-        float start = audioSource.volume;
+        float start = musicAudioSource.volume;
         while (currentTIme < 2f)
         {
             currentTIme += Time.deltaTime;
-            audioSource.volume = Mathf.Lerp(start, 0, currentTIme / 2f);
-            if(audioSource.volume == 0)
+            musicAudioSource.volume = Mathf.Lerp(start, 0, currentTIme / 2f);
+            if(musicAudioSource.volume == 0)
             {
-                audioSource.Stop();
+                musicAudioSource.Stop();
                 ChangeTrackWithoutFade(state);
-                audioSource.volume = start;
+                musicAudioSource.volume = start;
+                //audioSource.Play();
+            }
+            yield return null;
+        }
+        yield break;
+    }
+    public void ChangeAmbianceTrackWithoutFade(string tag)
+    {
+        var clipToPlay = ambianceTracks.Find(ambianceTracks => ambianceTracks.trackName == tag);
+        ambianceAudioSource.clip = clipToPlay.track;
+        ambianceAudioSource.Play();
+        ambianceAudioSource.volume = clipToPlay.volume;
+    }
+
+    public void ChangeAmbianceTrackForState(string state)
+    {
+        StartCoroutine(FadeOutAndPlayNextTrack(state));
+    }
+
+    public IEnumerator FadeOutAndPlayNextAmbianceTrack(string state)
+    {
+        float currentTIme = 0;
+        float start = ambianceAudioSource.volume;
+        while (currentTIme < 2f)
+        {
+            currentTIme += Time.deltaTime;
+            ambianceAudioSource.volume = Mathf.Lerp(start, 0, currentTIme / 2f);
+            if (ambianceAudioSource.volume == 0)
+            {
+                ambianceAudioSource.Stop();
+                ChangeTrackWithoutFade(state);
+                ambianceAudioSource.volume = start;
                 //audioSource.Play();
             }
             yield return null;

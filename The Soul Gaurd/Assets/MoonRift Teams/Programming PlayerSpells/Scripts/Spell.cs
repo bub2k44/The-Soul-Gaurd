e4ac61751a7119a637e596f07317443b1cec5bc1
bub2k44 <bub2k44@hotmail.com>
2021-02-02 23:01:42 -0700

    Rabbit Player bug fix

---
+++ The Soul Gaurd/Assets/MoonRift Teams/Programming PlayerSpells/Scripts/Spell.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Spell : IUseable, IMoveable
+{
+    [SerializeField]
+    private string name;
+
+    [SerializeField]
+    private int damage;
+
+    [SerializeField]
+    private Sprite icon;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float castTime;
+
+    [SerializeField]
+    private GameObject spellPrefab;
+
+    [SerializeField]
+    private Color barColor;
+
+    public string MyName { get => name; }
+    public int MyDamage { get => damage; }
+    public Sprite MyIcon { get => icon; }
+    public float MySpeed { get => speed; }
+    public float MyCastTime { get => castTime; }
+    public GameObject MySpellPrefab { get => spellPrefab; }
+    public Color MyBarColor { get => barColor; }
+
+    public void Use()
+    {
+        AgentController.MyInstance.CastSpell(MyName);
+    }
+}

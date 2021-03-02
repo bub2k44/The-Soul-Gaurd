8218f4ef6627dae21893ed5a7b30637cd4e6671d
albertgs09 <65637580+albertgs09@users.noreply.github.com>
2021-03-01 23:06:06 -0600

    FollowAIForBoss

---
+++ The Soul Gaurd/Assets/MoonRift Teams/Programming AI StateMachine/WorkStations/Albert/Albert'sBossBattleFolder/BossAIMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAIMovement : MonoBehaviour
+{
+    public Transform player;
+
+
+    public float MoveSpeed = 4;
+    public float rotateSpeed = 3;
+    public float maxDist = 10;
+    public float minDist = 5;
+    
+    bool playerClose = false;
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Path();
+    }
+
+    void Path()
+    {
+
+        transform.LookAt(player);
+        if (Vector3.Distance(transform.position, player.position) > maxDist)
+        {
+            playerClose = false;
+        }
+        if (Vector3.Distance(transform.position, player.position) >= minDist && playerClose == false)
+        {
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+           
+
+            Debug.Log("far");
+        }
+
+        if (Vector3.Distance(transform.position, player.position) <= maxDist)
+        {
+            
+            Debug.Log("Close");
+            
+        }
+
+
+    }
+}

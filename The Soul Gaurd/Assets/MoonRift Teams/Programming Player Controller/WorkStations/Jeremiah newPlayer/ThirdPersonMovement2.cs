7aa0f2955ee021916f6b549380b2fa08a9aa670e
bub2k44 <bub2k44@hotmail.com>
2021-03-01 23:19:45 -0700

    playerController V10

---
+++ The Soul Gaurd/Assets/MoonRift Teams/Programming Player Controller/WorkStations/Jeremiah newPlayer/ThirdPersonMovement2.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement2 : MonoBehaviour
+{
+    public Camera myCamera;
+
+    public float speed = 2f;
+    public float sprintSpeed = 5f;
+    public float rotationSpeed = 15f;
+    public float animationBlendSpeed = 6f;
+    public float jumpSpeed = 7f;
+
+    private CharacterController myController;
+    private Animator anim;
+
+    private float desiredRotation = 0f;
+    private float desiredAnimationSpeed = 0f;
+
+    private float speedY = 0f;
+    private float gravity = -9.81f;
+
+    private bool isSprinting = false;
+    public bool isJumping = false;
+
+    public bool canDoubleJump = false;
+    public float doubleJumpMultiplier = .8f;
+
+    private void Start()
+    {
+        myController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            isJumping = true;
+            anim.SetTrigger("jump");
+
+            speedY += jumpSpeed;
+        }
+
+        //if (myController.isGrounded)
+        //{
+        //    canDoubleJump = true;
+
+            
+        //}
+        //else
+        //{
+        //    if (Input.GetButtonDown("Jump") && canDoubleJump)
+        //    {
+        //        isJumping = true;
+        //        anim.SetTrigger("jump");
+
+        //        speedY += jumpSpeed *doubleJumpMultiplier;
+        //        canDoubleJump = false;
+        //    }
+        //}
+
+        
+
+        if (!myController.isGrounded)
+        {
+            speedY += gravity * Time.deltaTime;
+        }
+        else if (speedY < 0)
+        {
+            speedY = 0;
+        }
+
+        anim.SetFloat("speedY", speedY / jumpSpeed);
+
+        if (isJumping && speedY < 0)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.5f, LayerMask.GetMask("Default")))
+            {
+                isJumping = false;
+                anim.SetTrigger("land");
+            }
+        }
+
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+
+        Vector3 movement = new Vector3(x, 0, z).normalized;
+
+        Vector3 rotatedMovement = Quaternion.Euler(0, myCamera.transform.rotation.eulerAngles.y, 0) * movement;
+
+        Vector3 verticalMovement = Vector3.up * speedY;
+
+        myController.Move((verticalMovement + (rotatedMovement * (isSprinting ? sprintSpeed : speed))) * Time.deltaTime);
+
+        if (rotatedMovement.magnitude > 0)
+        {
+            desiredRotation = Mathf.Atan2(rotatedMovement.x, rotatedMovement.z) * Mathf.Rad2Deg;
+            //anim.SetFloat("speed", 1);
+            //desiredAnimationSpeed = 1;
+            desiredAnimationSpeed = isSprinting ? 1 : 0.5f;
+        }
+        else
+        {
+            //anim.SetFloat("speed", 0);
+            desiredAnimationSpeed = 0;
+        }
+
+        anim.SetFloat("speed", Mathf.Lerp(anim.GetFloat("speed"), desiredAnimationSpeed, animationBlendSpeed * Time.deltaTime));
+
+        Quaternion currentRotation = transform.rotation;
+        Quaternion targerRotation = Quaternion.Euler(0, desiredRotation, 0);
+
+        transform.rotation = Quaternion.Lerp(currentRotation, targerRotation, rotationSpeed * Time.deltaTime);
+    }
+}

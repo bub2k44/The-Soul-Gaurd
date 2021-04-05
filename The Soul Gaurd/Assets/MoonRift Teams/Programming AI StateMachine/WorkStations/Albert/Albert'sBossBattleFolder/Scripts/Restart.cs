e1358cb77811d8f2aa9a45178df21590d613f49b
albertgs09 <65637580+albertgs09@users.noreply.github.com>
2021-04-05 14:39:09 -0500

    BossDemo

--- The Soul Gaurd/Assets/MoonRift Teams/Programming AI StateMachine/WorkStations/Albert/Albert'sBossBattleFolder/Scripts/Restart.cs
+++ The Soul Gaurd/Assets/MoonRift Teams/Programming AI StateMachine/WorkStations/Albert/Albert'sBossBattleFolder/Scripts/Restart.cs
@@ -9,23 +9,33 @@ public class Restart : MonoBehaviour
     public Transform playerPoint, bossPoint, playerPoint2, bossPoint2;
     public BossAIMovement bossScript;
     public ChaseScript chaseScript;
+    public ThirdPersonMovement playerMovement;
     public GameObject chaseCollider;
     [SerializeField] int checkpoint;
 
     public void Start()
     {
-       PlayerPrefs.SetInt("Checkpoint", 0);
+      // PlayerPrefs.SetInt("Checkpoint", 0);
         checkpoint = PlayerPrefs.GetInt("Checkpoint");
 
        
         if(checkpoint == 1)
         {
-            RestartFromCheckPoint();
+            player.position = playerPoint.position;
+            boss.position = bossPoint.position;
+            chaseCollider.SetActive(false);
+            bossScript.enabled = true;
+            chaseScript.enabled = false;
+            playerMovement.rabbitCam.m_Priority = 0;
+            playerMovement.wolfCam.m_Priority = 1;
+            playerMovement.rabbit.SetActive(false);
+            playerMovement.wolf.SetActive(true);
         }
-       
+      
     }
     public void Update()
     {
+        checkpoint = PlayerPrefs.GetInt("Checkpoint");
 
         if (Input.GetKeyDown(KeyCode.RightShift))
         {
@@ -38,14 +48,21 @@ public class Restart : MonoBehaviour
     {
         Application.LoadLevel(Application.loadedLevel);
         Debug.Log(checkpoint);
-    }   
+    }
 
+    public void RestartWholeLevel()
+    {
+        PlayerPrefs.SetInt("Checkpoint", 0);
+
+        RestartLevel();
+    }
+    public void Checkpoint1()
+    {
+        PlayerPrefs.SetInt("Checkpoint", 1);
+        RestartLevel();
+    }
     public void RestartFromCheckPoint()
     {
-        player.position = playerPoint.position;
-        boss.position = bossPoint.position;
-        chaseCollider.SetActive(false);
-        bossScript.enabled = true;
-        chaseScript.enabled = false;
+        
     }
 }
